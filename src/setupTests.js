6d3215d47dc96feee16279f67eae32c25d7efb25
Aleh Krauchanka <Aleh.Krauchanka1@ibm.com>
2023-05-26 01:55:13 +0200

    Import enzyme, add its setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,16 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import 'regenerator-runtime/runtime';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
+
+global.Promise.prototype.isCancelled = jest.fn(() => false);
+global.Promise.prototype.cancel = jest.fn();
+
