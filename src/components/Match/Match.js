74a30cc14608849a7356a220dfa1a69f33032a11
Aleh Krauchanka <Aleh.Krauchanka1@ibm.com>
2023-06-01 01:46:57 +0200

    Make Match component inputs as a counters, implements remove button.

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Match = ({ id, currentMatches, setCurrentMatches }) => {
+    const [homeTeamScore, setHomeTeamScore] = useState(0);
+    const [awayTeamScore, setAwayTeamScore] = useState(0);
+
     return (
        <div id={`matchField_${id}`}>
+           <div id='homeTeam'>
+               <label htmlFor='points'>
+                   {currentMatches[id].homeTeam}
+               </label>
+               <input
+                   type='number'
+                   id='homeTeamPoints'
+                   name='points'
+                   min='0'
+                   step='1'
+                   value={homeTeamScore}
+                   onChange={event => setHomeTeamScore(event.target.value)}
+               />
+           </div>
+           &nbsp;
+           :
+           &nbsp;
+           <div id='awayTeam'>
+               <input
+                   type='number'
+                   id='awayTeamPoints'
+                   name='points'
+                   min='0'
+                   step='1'
+                   value={awayTeamScore}
+                   onChange={event => setAwayTeamScore(event.target.value)}
+               />
+               <label htmlFor='points'>
+                   {currentMatches[id].awayTeam}
+               </label>
+           </div>
+           &nbsp;
            <button
                id='removeMatch'
                onClick={() => {
-                   delete currentMatches[id];
-                   setCurrentMatches(currentMatches);
+                   const clone = { ...currentMatches };
+                   delete clone[id];
+                   setCurrentMatches(clone);
                }}
            >
                Remove
            </button>
-           <span id='homeTeam'>{currentMatches[id].homeTeam}</span>
-           &nbsp;
-           :
-           &nbsp;
-           <span id='awayTeam'>{currentMatches[id].awayTeam}</span>
            <button id='finishMatch'>
                Finish
            </button>
--- src/components/Match/Match.test.js
+++ src/components/Match/Match.test.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 
 import Matches from './Match';
 
 
+const testMatch = {
+    id: 'a_b',
+    'a_b': {
+        id: 'a_b',
+        homeTeam: 'testHomeTeamName',
+        homeTeamScore: 0,
+        awayTeam: 'testAwayTeamName',
+        awayTeamScore: 0
+    }
+};
+const mockSetCurrentMatches = jest.fn();
+
 describe('Current Match component', () => {
     it('renders default Match values', async () => {
-        const mockSetCurrentMatches = jest.fn();
-        const testMatch = {
-            id: 'a_b',
-            'a_b': {
-                id: 'a_b',
-                homeTeam: 'testHomeTeamName',
-                homeTeamScore: 0,
-                awayTeam: 'testAwayTeamName',
-                awayTeamScore: 0
-            }
-        };
-
         const container = mount(
             <Matches
                 id={testMatch.id}
@@ -25,10 +26,54 @@ describe('Current Match component', () => {
                 setCurrentMatches={mockSetCurrentMatches} />
         );
 
-        expect(container.find('button').at(0).prop('id')).toEqual('removeMatch');
         expect(container.find('div').at(0).prop('id')).toEqual(`matchField_${testMatch.id}`);
-        expect(container.find('span#homeTeam').at(0).text()).toEqual('testHomeTeamName');
-        expect(container.find('span#awayTeam').at(0).text()).toEqual('testAwayTeamName');
+        expect(container.find('div#homeTeam label').at(0).text()).toEqual('testHomeTeamName');
+        expect(container.find('div#awayTeam label').at(0).text()).toEqual('testAwayTeamName');
+        expect(container.find('button').at(0).prop('id')).toEqual('removeMatch');
         expect(container.find('button').at(1).prop('id')).toEqual('finishMatch');
     });
-})
+
+    it('change Match values', async () => {
+        let container;
+
+        await act(async () => {
+            container = await mount(
+                <Matches
+                    id={testMatch.id}
+                    currentMatches={testMatch}
+                    setCurrentMatches={mockSetCurrentMatches} />
+            );
+        });
+        container.update();
+
+        await act(async () => {
+            container.find('input#homeTeamPoints').at(0).simulate('change', { persist: jest.fn(), target: { value: 10 } });
+            container.find('input#awayTeamPoints').at(0).simulate('change', { persist: jest.fn(), target: { value: 5 } });
+        });
+        container.update();
+
+        expect(container.find('input#homeTeamPoints').at(0).prop('value')).toEqual(10);
+        expect(container.find('input#awayTeamPoints').at(0).prop('value')).toEqual(5);
+    });
+
+    it('remove current Match', async () => {
+        let container;
+
+        await act(async () => {
+            container = await mount(
+                <Matches
+                    id={testMatch.id}
+                    currentMatches={testMatch}
+                    setCurrentMatches={mockSetCurrentMatches} />
+            );
+        });
+        container.update();
+
+        await act(async () => {
+            container.find('button#removeMatch').at(0).simulate('click');
+        });
+        container.update();
+
+        expect(mockSetCurrentMatches).toHaveBeenCalled();
+    });
+});
