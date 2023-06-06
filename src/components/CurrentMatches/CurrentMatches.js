7d619027a8c1693aae113ea78d24dedfc96f22c6
Aleh Krauchanka <Aleh.Krauchanka1@ibm.com>
2023-06-06 14:12:25 +0200

    AddField, CurrentMatches and Match styles.

--- src/components/CurrentMatches/CurrentMatches.js
+++ src/components/CurrentMatches/CurrentMatches.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import style from './CurrentMatches.module.scss'
 import Match from '../Match/Match';
 
 const CurrentMatches = ({ currentMatches, setCurrentMatches, setMatchesSummary }) => {
     return (
-       <div id='currentMatchesField'>
+       <div id='currentMatchesField' className={style.currentMatches}>
            <h3>Current Matches</h3>
            {Object.values(currentMatches)
                ?.map(currentMatch => {
